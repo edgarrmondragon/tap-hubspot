cab884738682248faed53535c21c0fda710937c6
Stephane Burwash <stephane.burwash@potloc.com>
2022-05-24 10:59:00 -0400

    Updated campaigns schema

--- tap_hubspot/schemas/marketing/Campaigns.py
+++ tap_hubspot/schemas/marketing/Campaigns.py
@@ -2,8 +2,11 @@ from singer_sdk import typing as th  # JSON Schema typing helpers
 
 schema = th.PropertiesList(
     th.Property("id", th.IntegerType),
+    th.Property("appId", th.IntegerType),
     th.Property("appName", th.StringType),
     th.Property("contentId", th.IntegerType),
+    th.Property("name", th.StringType),
+    th.Property("subject", th.StringType),
     th.Property("counters",
         th.ObjectType(
             th.Property("processed", th.IntegerType),

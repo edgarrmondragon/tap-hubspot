be6c7c47f0bfb014bb2509f13d05e6583a0ca2f1
Stephane Burwash <stephane.burwash@potloc.com>
2022-05-24 14:32:27 -0400

    Created web analytics stream schema

--- tap_hubspot/schemas/events/WebAnalytics.py
+++ tap_hubspot/schemas/events/WebAnalytics.py
@@ -1,7 +1,19 @@
 from singer_sdk import typing as th  # JSON Schema typing helpers
 
 schema = th.PropertiesList(
-    th.Property("id", th.NumberType),
-    th.Property("appId", th.NumberType),
-    th.Property("appName", th.StringType),
+    th.Property("objectType", th.StringType),
+    th.Property("objectId", th.StringType),
+    th.Property("eventType", th.StringType),
+    th.Property("occurredAt", th.DateTimeType),
+    th.Property("id", th.StringType),
+    th.Property("Properties",
+        th.ObjectType(
+            th.Property("hs_action_execution_index", th.StringType),
+            th.Property("hs_flow_id", th.StringType),
+            th.Property("hs_action_executed_timestamp", th.StringType),
+            th.Property("hs_action_id", th.StringType),
+            th.Property("hs_enrollment_id", th.StringType),
+            th.Property("hs_delayed_object_type", th.StringType)
+        )
+    ),
 ).to_dict()

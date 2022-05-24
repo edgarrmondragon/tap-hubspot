c0310f169390afc8a566d8e31ed2dd2b815d4e32
Stephane Burwash <stephane.burwash@potloc.com>
2022-05-24 16:38:33 -0400

    Added datetime management

--- tap_hubspot/analytics_streams.py
+++ tap_hubspot/analytics_streams.py
@@ -8,6 +8,7 @@ import json
 from dateutil import parser
 import datetime, pytz
 import time
+from datetime import datetime
 
 from pathlib import Path
 from typing import Any, Dict, Optional, Union, List, Iterable
@@ -37,10 +38,31 @@ class AnalyticsStream(HubspotStream):
     records_jsonpath = "$.[*]"  # Or override `parse_response`.
     schema_filepath = ""
 
+    def post_process(self, row: dict, context: Optional[dict]) -> dict:
+        """As needed, append or transform raw data to match expected structure.
+        Returns row, or None if row is to be excluded"""
+
+        if self.replication_key:
+            if row[self.replication_key] <= self.get_starting_timestamp(context).astimezone(pytz.utc):
+                return None
+        return row
+
 class AnalyticsViewsStream(AnalyticsStream):
     name = "analytics_views_v2"
     path = "/analytics/v2/views"
     primary_keys = ["id"]
     schema = AnalyticsViews.schema
     replication_key = "updatedDate"
+
+    def parse_response(self, response: requests.Response) -> Iterable[dict]:
+        """Parse the response and return an iterator of result rows."""
+        data = response.json()
+        ret = []
+        for d in data:
+            val = d
+            val["updatedDate"] = datetime.strptime(d["updatedDate"], "%Y-%m-%d").astimezone(pytz.utc)
+            ret.append(val)
+        data = ret
+        yield from extract_jsonpath(self.records_jsonpath, input=data)
+
 
